1bfb69906cb539cf1629a41789e9b70d00023242
Hiden1807 <ngalamulumejonas18@gmail.com>
2026-07-18 10:02:06 +0100

    Fix duplication ALLOW_LEGACY_MARIADB_104 dans settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -115,8 +115,6 @@ if os.getenv("DATABASE_URL"):
         )
     }
 elif os.getenv("DB_ENGINE", "sqlite").lower() == "mysql":
-
-if os.getenv("DB_ENGINE", "sqlite").lower() == "mysql":
     import pymysql
 
     pymysql.install_as_MySQLdb()
